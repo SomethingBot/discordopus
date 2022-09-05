4231bef63edbaddb81881e91878272d146b9d12e
SlightlySideways <82488311+SlightlySideways@users.noreply.github.com>
2022-09-05 11:55:32 -0500

    linting improvements

--- discordopus.go
+++ discordopus.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"os/exec"
 
@@ -13,33 +14,38 @@ import (
 	"layeh.com/gopus"
 )
 
+var ErrNoAudioStream = errors.New("no audio streams found")
+
 func GetAudioStream(url string) (io.ReadCloser, error) {
-	client := youtube.Client{}
+	client := youtube.Client{Debug: false, HTTPClient: http.DefaultClient}
+
 	video, err := client.GetVideo(url)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not get video (%w)", err)
 	}
 
 	formatList := video.Formats.Type("audio")
 	formatList.Sort()
 
 	if len(formatList) == 0 {
-		return nil, errors.New("no audio streams found")
+		return nil, ErrNoAudioStream
 	}
 
-	var rc io.ReadCloser
-	rc, _, err = client.GetStream(video, &formatList[0])
+	var readCloser io.ReadCloser
+
+	readCloser, _, err = client.GetStream(video, &formatList[0])
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not get audio stream (%w)", err)
 	}
 
-	return rc, nil
+	return readCloser, nil
 }
 
 type MultiCloser []io.Closer
 
 func (mc *MultiCloser) Close() error {
 	var err error
+
 	for _, c := range []io.Closer(*mc) {
 		err2 := c.Close()
 		if err2 != nil {
@@ -63,7 +69,7 @@ type CloseReader struct {
 const channels = 2
 const frameRate = 48000
 const frameSize = 960
-const maxBytes = (frameSize * 2) * 2
+const maxBytes = (frameSize * channels) * channels
 
 func LiveConvertAudioStreamToS16LE(audioStream io.Reader) (io.ReadCloser, error) {
 	/* #nosec G204 */
@@ -73,20 +79,19 @@ func LiveConvertAudioStreamToS16LE(audioStream io.Reader) (io.ReadCloser, error)
 
 	readCloser, err := ffmpeg.StdoutPipe()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not get StdoutPipe from ffmpeg (%w)", err)
 	}
 
 	err = ffmpeg.Start()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("ffmpeg start error (%w)", err)
 	}
 
-	var fc functionCloser
-	fc = ffmpeg.Process.Kill
+	fc := functionCloser(ffmpeg.Process.Kill)
 
 	mc := MultiCloser([]io.Closer{&fc, readCloser})
 
-	return &CloseReader{Closer: &mc, Reader: readCloser}, err
+	return &CloseReader{Closer: &mc, Reader: readCloser}, nil
 }
 
 type PCMData struct {
@@ -102,28 +107,30 @@ func ConvertS16LEBytesToPCM(reader io.Reader) chan PCMData {
 
 		enc, err := gopus.NewEncoder(frameRate, channels, gopus.Audio)
 		if err != nil {
-			pcmData <- PCMData{Error: err}
+			pcmData <- PCMData{Error: fmt.Errorf("coud not get new gopus encoder (%w)", err)}
 			return
 		}
 
 		buf := make([]int16, frameSize*channels)
+
 		var opusData []byte
 		for {
 			err = binary.Read(reader, binary.LittleEndian, &buf)
-			if err == io.EOF || err == io.ErrUnexpectedEOF {
+			if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
 				return
 			}
 			if err != nil {
-				pcmData <- PCMData{Error: err}
+				pcmData <- PCMData{Error: fmt.Errorf("could not binary read from input reader while converting to PCM (%w)", err)}
 				return
 			}
 			opusData, err = enc.Encode(buf, frameSize, maxBytes)
 			if err != nil {
-				pcmData <- PCMData{Error: err}
+				pcmData <- PCMData{Error: fmt.Errorf("could not encode opus data (%w)", err)}
 				return
 			}
 			pcmData <- PCMData{Data: opusData}
 		}
 	}(pcmData)
+
 	return pcmData
 }
